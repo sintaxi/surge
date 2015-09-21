b1048a28040e9f365de0f88893423f807fd99745
Kenneth Ormandy <kenneth@chloi.io>
2015-09-21 12:56:58 -0700

    Adds initial tests

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,38 @@
-describe("surge", function(){
+var nixt = require('nixt')
+var should = require('should')
+var surge = 'node ./lib/cli.js '
+var opts = {
+  colors: false,
+  newlines: false
+}
+var pkg = require('../package.json')
 
-  it("should be cool", function(done){
+describe('surge', function () {
+
+  it('should be cool', function (done) {
     done()
+  })
+
+  describe('should return the version number', function (done) {
+
+    it('`surge --version`', function (done) {
+      nixt(opts)
+        .run(surge + '--version')
+        .expect(function(result) {
+          should(result.stdout).be.equal(pkg.version)
+        })
+        .end(done)
+    })
+
+    it('`surge -V`', function (done) {
+      nixt(opts)
+        .run(surge + '-V')
+        .expect(function(result) {
+          should(result.stdout).be.equal(pkg.version)
+        })
+        .end(done)
+    })
+
   })
 
 })
