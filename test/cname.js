71123e7ed368d2582ff7bd9b12b341aa353785c6
Kenneth Ormandy <kenneth@chloi.io>
2015-09-28 15:25:18 -0700

    Adds CNAME and domain variation tests

---
+++ test/cname.js
@@ -0,0 +1,44 @@
+var nixt = require('nixt')
+var should = require('should')
+var pkg = require('../package.json')
+
+var surge = 'node ./lib/cli.js '
+var opts = {
+  colors: false,
+  newlines: false
+}
+
+describe('CNAME', function (done) {
+
+  it('`surge` with CNAME file', function (done) {
+    this.timeout(5000)
+    nixt(opts)
+      .run(surge + './test/fixtures/cli-test-2.surge.sh')
+      .expect(function (result) {
+        should(result.stdout).match(/2 file/)
+        should(result.stdout).match(/Success! Project is published and running at cli-test-2/)
+      })
+      .end(done)
+  })
+  it('`surge` with CNAME file and protocol', function (done) {
+    this.timeout(5000)
+    nixt(opts)
+      .run(surge + './test/fixtures/cli-test-3.surge.sh')
+      .expect(function (result) {
+        should(result.stdout).match(/2 file/)
+        should(result.stdout).match(/Success! Project is published and running at cli-test-3/)
+      })
+      .end(done)
+  })
+  it('`surge` with CNAME file and subdomain', function (done) {
+    this.timeout(5000)
+    nixt(opts)
+      .run(surge + './test/fixtures/cli-test-4.surge.sh')
+      .expect(function (result) {
+        should(result.stdout).match(/2 file/)
+        should(result.stdout).match(/Success! Project is published and running at www\.cli-test-4/)
+      })
+      .end(done)
+  })
+
+})
