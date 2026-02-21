fb55ec4da0aa74fcc5611f07dea586d061e230a7
Brock Whitten <brock@sintaxi.com>
2026-02-20 21:24:25 -0800

    removes commander and yargs tests

--- test/actions.js
+++ test/actions.js
@@ -1,11 +1,9 @@
 var should = require('should')
-var commander = require('commander')
 var nixt = require('nixt')
 var Surge = require('../')
 var surge = new Surge
 var pkg = require('../package.json')
 var minimist = 'node ./test/fixtures/bin/minimist.js'
-var yargs = 'node ./test/fixtures/bin/yargs.js'
 var hooks = {}
 
 describe('actions', function (done) {
@@ -17,46 +15,6 @@ describe('actions', function (done) {
       done()
     })
 
-    it('commander', function (done) {
-      var program = commander
-      program
-        .command('login')
-        .action(surge.login(hooks))
-      should(program.commands.length).equal(1)
-      done()
-    })
-
-    it('commander without args', function (done) {
-      this.timeout(10000)
-      var testDomain = 'cli-actions-test-' + Date.now() + '.surge.sh'
-      var commander = 'node ./test/fixtures/bin/commander-no-args.js'
-      nixt({ colors: false })
-        .exec(commander + ' logout')
-        .run(commander + ' up')
-        .on(/.*email:.*/).respond('brock+test@chloi.io\n')
-        .on(/.*password:.*/).respond('12345\n')
-        .on(/.*domain:.*/).respond(testDomain + '\n')
-        .expect(function (result) {
-          should(result.stdout).match(/Success/)
-        })
-        .exec(commander + ' logout')
-        .end(done)
-    })
-
-    it('commander without hooks object', function (done) {
-      var commander = 'node ./test/fixtures/bin/commander-no-hooks.js'
-      nixt({ colors: false })
-        .exec(commander + ' logout')
-        .run(commander + ' login')
-        .on(/.*email:.*/).respond('brock+test@chloi.io\n')
-        .on(/.*password:.*/).respond('12345\n')
-        .expect(function (result) {
-          should(result.stdout).match(/Success - Logged in as brock\+test@chloi\.io/)
-        })
-        .exec(commander + ' logout')
-        .end(done)
-    })
-
     it('minimist', function (done) {
       nixt({ colors: false })
         .run(minimist + ' login')
@@ -66,18 +24,6 @@ describe('actions', function (done) {
           should(result.stdout).match(/Success - Logged in as brock\+test@chloi\.io/)
         })
         .exec(minimist + ' logout')
-        .end(done)
-    })
-
-    it('yargs', function (done) {
-      nixt({ colors: false })
-        .run(yargs + ' login')
-        .on(/.*email:.*/).respond('brock+test@chloi.io\n')
-        .on(/.*password:.*/).respond('12345\n')
-        .expect(function (result) {
-          should(result.stdout).match(/Success - Logged in as brock\+test@chloi\.io/)
-        })
-        .exec(yargs + ' logout')
         .end(done)
     })
   })
